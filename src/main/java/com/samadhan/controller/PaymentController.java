dfea051e2b290248b05c93d22de262f5f6fe9036
amit96pal <amit96pal.ap@gmail.com>
2024-08-04 11:25:09 +0530

    added code for subscription response

--- src/main/java/com/samadhan/controller/PaymentController.java
+++ src/main/java/com/samadhan/controller/PaymentController.java
@@ -3,23 +3,27 @@ package com.samadhan.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.razorpay.RazorpayClient;
 import com.razorpay.RazorpayException;
+import com.samadhan.response.SubscriptionResponse;
+import com.samadhan.service.PaymentServiceImpl;
 import com.samadhan.util.Utils;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.samadhan.dto.payment.PaymentInvoiceRequest;
 import com.samadhan.service.PaymentService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/pay")
 public class PaymentController {
 
-//	 @Autowired
-//	 PaymentService service;
+	@Autowired
+	private PaymentServiceImpl paymentService;
 
 	 @Autowired
 	 private ObjectMapper mapper;
-
 
 
 	@PostMapping("/generate-new-invoice")
@@ -36,6 +40,13 @@ public class PaymentController {
 		RazorpayClient razorpayClient = Utils.getPaymentClient();
 		com.razorpay.Invoice fetch = razorpayClient.invoices.fetch(invoiceId);
 		return "invoice data "+fetch.toJson();
+	}
+
+
+	@GetMapping("/subscriptions")
+	public ResponseEntity<List<SubscriptionResponse>> getSubscriptions() {
+		List<SubscriptionResponse> allSubscriptions = paymentService.getAllSubscriptions();
+		return ResponseEntity.ok(allSubscriptions);
 	}
 
 	
