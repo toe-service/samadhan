6eafe3e6a979bb019645f7724473fd9cb5906dc5
amit96pal <amit96pal.ap@gmail.com>
2025-08-27 21:05:44 +0530

    added loggers

--- src/main/java/com/samadhan/controller/UserLoginController.java
+++ src/main/java/com/samadhan/controller/UserLoginController.java
@@ -7,6 +7,8 @@ import com.samadhan.request.UserLoginRequest;
 import com.samadhan.response.GetOtpResponse;
 import com.samadhan.response.UserLoginResponse;
 import com.samadhan.service.LoginService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,17 +17,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/toe-user-service")
 public class UserLoginController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserLoginController.class);
+
     @Autowired
     private LoginService loginService;
 
     @GetMapping("/get-otp/{mobileNumber}")
     public ResponseEntity<GetOtpResponse> getOtp(@PathVariable("mobileNumber") String mobileNumber) throws NotificationException {
+        logger.info("Mobile number to generate opt is {}", mobileNumber);
         GetOtpResponse getOtpResponse = loginService.generateAndSendOtp(mobileNumber);
         return ResponseEntity.ok(getOtpResponse);
     }
 
     @PostMapping("/login")
     public ResponseEntity<UserLoginResponse> loginUser(@RequestBody UserLoginRequest userLoginRequest) {
+        logger.info("User login request is {}", userLoginRequest.toString());
         if (userLoginRequest.getOtp() == 1234) {
             return ResponseEntity.ok(UserLoginResponse.of(true, AppConstant.USER_LOGIN_SUCCESSFUL, UserLoginData.of("amit")));
         } else {
