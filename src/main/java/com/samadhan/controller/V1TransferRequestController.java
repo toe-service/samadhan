48221c817a118e3047a38165b330d562a2e6fda3
amit96pal <amit96pal.ap@gmail.com>
2026-03-21 11:47:56 +0530

    code update-1

--- src/main/java/com/samadhan/controller/V1TransferRequestController.java
+++ src/main/java/com/samadhan/controller/V1TransferRequestController.java
@@ -1,8 +1,6 @@
 package com.samadhan.controller;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,14 +14,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.samadhan.entity.Ride;
 import com.samadhan.entity.TransferRequestDetails;
-import com.samadhan.entity.VehicleTransfer;
 import com.samadhan.service.TransferRequestService;
 
 @RestController
 @RequestMapping("/transfer")
-public class TransferRequestController {
+public class V1TransferRequestController {
 	
 
 @Autowired
