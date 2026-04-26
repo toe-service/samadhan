849cd5f245e3587deeb888f1e5b5ae8a531899a1
shiva <shivankhal786@gmail.com>
2026-04-26 12:30:42 +0530

    cors issue for vendor frontend

--- src/main/java/com/samadhan/config/CorsConfig.java
+++ src/main/java/com/samadhan/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/v1/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:8081",  "https://transfer-vendor-production.up.railway.app")
+                .allowedOrigins("http://localhost:5173", "http://localhost:8081",  "https://transfer-vendor-production.up.railway.app" )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
