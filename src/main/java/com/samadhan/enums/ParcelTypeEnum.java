63809f6bd5548dd74c1d0f305d210ceadf4d35e0
shiva <shivankhal786@gmail.com>
2026-05-16 13:15:51 +0530

    delete request issue-1

--- src/main/java/com/samadhan/enums/ParcelTypeEnum.java
+++ src/main/java/com/samadhan/enums/ParcelTypeEnum.java
@@ -1,15 +1,15 @@
 package com.samadhan.enums;
 
-public enum VihicleModelEnum {
+public enum ParcelTypeEnum {
 	
-	Sedan(0,"Sedan"),
-	SUV(1,"SUV"),
-	Hatchback(2,"Hatchback"),
-	Luxury(3,"Luxury");
+	Car(0,"Car"),
+	Bike(1,"Bike"),
+	Parcel(2,"Parcel");
+	
 	
 	int id;
 	String type;
-	private VihicleModelEnum(int id, String type) {
+	private ParcelTypeEnum(int id, String type) {
 		this.id = id;
 		this.type = type;
 	}
@@ -21,7 +21,7 @@ public enum VihicleModelEnum {
 	}
 	
 	public static Integer getIdByType(String type) {
-		for(VihicleModelEnum b:values()) {
+		for(ParcelTypeEnum b:values()) {
 			if(b.getType().equalsIgnoreCase(type)) {
 				return b.getId();
 			}
@@ -30,7 +30,7 @@ public enum VihicleModelEnum {
 	}
 	
 	public static String getTypeById(int id) {
-		for(VihicleModelEnum b:values()) {
+		for(ParcelTypeEnum b:values()) {
 			if(b.getId()==id) {
 				return b.getType();
 			}
@@ -40,5 +40,3 @@ public enum VihicleModelEnum {
 	
 
 }
-
-
