0b6fa6baf79b8183a5595d7afd2e6c1030ca5022
shiva <shivankhal786@gmail.com>
2026-06-03 02:12:47 +0530

    changes as per new Requirement

---
+++ src/main/java/com/samadhan/entity/WalletTransaction.java
@@ -0,0 +1,93 @@
+package com.samadhan.entity;
+
+import lombok.Data;
+
+import java.util.List;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@Entity
+public class WalletTransaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double amount;
+
+    private String transactionType;
+
+    private String description;
+
+    private Long referenceId;
+
+    @ManyToOne
+    @JoinColumn(name = "vendor_id")
+    private TransferVendor vendor;
+    
+    @ManyToOne
+    @JoinColumn(name = "transfer_request_id")
+    private TransferRequestDetails transferRequestDetail;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public String getTransactionType() {
+		return transactionType;
+	}
+
+	public void setTransactionType(String transactionType) {
+		this.transactionType = transactionType;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Long getReferenceId() {
+		return referenceId;
+	}
+
+	public void setReferenceId(Long referenceId) {
+		this.referenceId = referenceId;
+	}
+
+	public TransferVendor getVendor() {
+		return vendor;
+	}
+
+	public void setVendor(TransferVendor vendor) {
+		this.vendor = vendor;
+	}
+
+	public TransferRequestDetails getTransferRequestDetail() {
+		return transferRequestDetail;
+	}
+
+	public void setTransferRequestDetail(TransferRequestDetails transferRequestDetail) {
+		this.transferRequestDetail = transferRequestDetail;
+	}
+    
+}
