039acfb005e09551d7678281b61679194ce1f4d9
shiva <shivankhal786@gmail.com>
2026-06-06 21:45:52 +0530

    subscription changes fix

---
+++ src/main/java/com/samadhan/dto/PaymentVerificationRequest.java
@@ -0,0 +1,49 @@
+package com.samadhan.dto;
+
+import lombok.Data;
+
+@Data
+public class PaymentVerificationRequest {
+
+    private Long vendorId;
+
+    private String razorpayPaymentId;
+
+    private String razorpayOrderId;
+
+    private String razorpaySignature;
+
+	public Long getVendorId() {
+		return vendorId;
+	}
+
+	public void setVendorId(Long vendorId) {
+		this.vendorId = vendorId;
+	}
+
+	public String getRazorpayPaymentId() {
+		return razorpayPaymentId;
+	}
+
+	public void setRazorpayPaymentId(String razorpayPaymentId) {
+		this.razorpayPaymentId = razorpayPaymentId;
+	}
+
+	public String getRazorpayOrderId() {
+		return razorpayOrderId;
+	}
+
+	public void setRazorpayOrderId(String razorpayOrderId) {
+		this.razorpayOrderId = razorpayOrderId;
+	}
+
+	public String getRazorpaySignature() {
+		return razorpaySignature;
+	}
+
+	public void setRazorpaySignature(String razorpaySignature) {
+		this.razorpaySignature = razorpaySignature;
+	}
+    
+    
+}
