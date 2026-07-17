14882257369007457cd827d7b8e35a171aeb9851
shiva <shivankhal786@gmail.com>
2026-07-18 02:53:18 +0530

    Book Vehicle implementation

---
+++ src/main/java/com/samadhan/dto/BookVehicleCostResponse.java
@@ -0,0 +1,87 @@
+package com.samadhan.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookVehicleCostResponse {
+
+    private String vehicle;
+
+    private Double rideCost;
+
+    private Double gst;
+
+    private Double loadingUnloading;
+
+    private Double packaging;
+
+    private Double totalCost;
+
+	public BookVehicleCostResponse(String vehicle, Double rideCost, Double gst, Double loadingUnloading,
+			Double packaging, Double totalCost) {
+		this.vehicle = vehicle;
+		this.rideCost = rideCost;
+		this.gst = gst;
+		this.loadingUnloading = loadingUnloading;
+		this.packaging = packaging;
+		this.totalCost = totalCost;
+	}
+
+	public BookVehicleCostResponse() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(String vehicle) {
+		this.vehicle = vehicle;
+	}
+
+	public Double getRideCost() {
+		return rideCost;
+	}
+
+	public void setRideCost(Double rideCost) {
+		this.rideCost = rideCost;
+	}
+
+	public Double getGst() {
+		return gst;
+	}
+
+	public void setGst(Double gst) {
+		this.gst = gst;
+	}
+
+	public Double getLoadingUnloading() {
+		return loadingUnloading;
+	}
+
+	public void setLoadingUnloading(Double loadingUnloading) {
+		this.loadingUnloading = loadingUnloading;
+	}
+
+	public Double getPackaging() {
+		return packaging;
+	}
+
+	public void setPackaging(Double packaging) {
+		this.packaging = packaging;
+	}
+
+	public Double getTotalCost() {
+		return totalCost;
+	}
+
+	public void setTotalCost(Double totalCost) {
+		this.totalCost = totalCost;
+	}
+}
