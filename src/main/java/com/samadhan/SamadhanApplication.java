e271553941288587b8ae5a1ee50dacef916b0b25
shiva <shivankhal786@gmail.com>
2026-08-01 15:39:52 +0530

    push notification implementation-3

--- src/main/java/com/samadhan/SamadhanApplication.java
+++ src/main/java/com/samadhan/SamadhanApplication.java
@@ -23,13 +23,40 @@ import java.util.Objects;
 @EnableCaching
 public class SamadhanApplication {
 
+//	@Bean
+//	FirebaseMessaging firebaseMessaging() throws IOException {
+//	    GoogleCredentials googleCredentials = GoogleCredentials.fromStream(new ClassPathResource("firebase.json").getInputStream());
+//	    FirebaseOptions firebaseOptions = FirebaseOptions.builder().setCredentials(googleCredentials).build();
+//	    FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "my-app");
+//	    return FirebaseMessaging.getInstance(app);
+//	  }
+	
 	@Bean
-	FirebaseMessaging firebaseMessaging() throws IOException {
-	    GoogleCredentials googleCredentials = GoogleCredentials.fromStream(new ClassPathResource("firebase.json").getInputStream());
-	    FirebaseOptions firebaseOptions = FirebaseOptions.builder().setCredentials(googleCredentials).build();
-	    FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "my-app");
+	public FirebaseMessaging firebaseMessaging() throws IOException {
+
+	    GoogleCredentials credentials =
+	            GoogleCredentials.fromStream(
+	                    new ClassPathResource("serviceAccountKey.json").getInputStream());
+
+	    FirebaseOptions options = FirebaseOptions.builder()
+	            .setCredentials(credentials)
+	            .build();
+
+	    FirebaseApp app;
+
+//	    if (FirebaseApp.getApps().isEmpty()) {
+//	        app = FirebaseApp.initializeApp(options, "my-app");
+//	    } else {
+//	        app = FirebaseApp.getApps().get(0);
+//	    }
+	    if (FirebaseApp.getApps().isEmpty()) {
+	        app = FirebaseApp.initializeApp(options);
+	    } else {
+	        app = FirebaseApp.getInstance();
+	    }
+
 	    return FirebaseMessaging.getInstance(app);
-	  }
+	}
 
 	public static void main(String[] args) throws IOException {
 		 TimeZone.setDefault(
